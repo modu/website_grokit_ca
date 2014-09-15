9b04b5d1a1f42a7188dcf91d5a5f08bce696da2f
david <reachme.web@outlook.com>
2014-09-14 22:54:18 -0700

    i++wq

--- articles/web_cs_review/BinarySearchTree_InorderSuccessor/bst_inordersuccessor.py
+++ articles/web_cs_review/BinarySearchTree_InorderSuccessor/bst_inordersuccessor.py
@@ -1,13 +1,16 @@
 
+import random
+
 import dcore.ds.bst as bst
 
-
 def firstLeftChildUp(n):
+    if n.parent is None:
+        return None
     while not bst.BST.isLeftChild(n, n.parent):
         n = n.parent
-        if n is None:
+        if n is None or n.parent is None:
             return None
-    return n
+    return n.parent
 
 def getInorderNext(n):
     #import pdb;pdb.set_trace()
@@ -15,9 +18,9 @@ def getInorderNext(n):
         if n.right is None:
             return None
         return bst.BST.leftmostChild(n.right)
-    elif not bst.BST.isLeftChild(n, n.parent):
+    elif bst.BST.isLeftChild(n, n.parent):
         if n.right is None:
-            return None
+            return n.parent 
         return bst.BST.leftmostChild(n.right)
     else:
         if n.right is not None:
@@ -25,16 +28,22 @@ def getInorderNext(n):
         else:
             return firstLeftChildUp(n.parent)
 
-tree = bst.buildRandomBST()
-tree.toGraph()
-"""
-treeArray = tree.asInorderArray()
-print(treeArray)
+nTest = 100
+for i in range(0, nTest):
+    tree = bst.buildRandomBST(10)
+    tree.toGraph()
+    treeArray = tree.asInorderArray()
+    print(treeArray)
 
-n = tree.getNodeFromValue(treeArray[ int(len(treeArray) / 2)])
-print(n)
+    pos = random.randint(0, len(treeArray)-1)
+    n = tree.getNodeFromValue(treeArray[ pos ])
+    print(n)
 
-nn = getInorderNext(n)
-print(nn)
+    nn = getInorderNext(n)
+    print(nn)
 
-"""
+    if len(treeArray) == pos + 1:
+        assert nn is None
+    else:
+        assert nn.value == treeArray[pos+1]
+
