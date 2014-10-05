9689156ebca1a324dd618fa6ec7f09a434d8f5f6
david <reachme.web@outlook.com>
2014-10-04 20:37:51 -0700

    ++

---
+++ articles/web_cs_review/BinarySearchTree_TraverseNonRecursive/inorder_norecurse.py
@@ -0,0 +1,35 @@
+
+import ds.bst as bst
+
+tree = bst.buildRandomBST(10)
+print(tree.asInorderArray())
+
+def visit(n):
+    print(n.value)
+
+def ino(root, visitFn = visit):
+    root.vl = False
+    root.vc = False
+    S = [root]
+
+    while len(S) > 0:
+        n = S.pop()
+
+        if n.vl == False:
+            n.vl = True
+            S.append(n)
+            if n.left is not None:
+                n.left.vl = False
+                n.left.vc = False
+                S.append(n.left)
+
+        elif n.vc == False:
+            n.vc = True
+            visitFn(n)
+
+            if n.right is not None:
+                n.right.vl = False
+                n.right.vc = False
+                S.append(n.right)
+
+ino(tree.root)
