66c6816741f7202447dc3544c9df67bd11ec8739
david <reachme.web@outlook.com>
2014-10-08 19:33:53 -0700

    fix errors

--- articles/web_cs_review/ShortestPathDijkstra/dijkstra.py
+++ articles/web_cs_review/ShortestPathDijkstra/dijkstra.py
@@ -19,38 +19,31 @@ def visitVertex(paths, path, edge, visited):
     paths.append( (path, cost) )
 
 def popLeastCostPathAndEdge(paths, visited):
-
-    fPath, minCost = paths[0]
-    minE = next(iter(fPath[-1].edges))
-    minCost += minE.weight
-    remPath = paths[0] 
+    minCost = None
 
     for path, cost in paths:
+        pathSoFar = path
         path = path[-1]
         for edge in path.edges:
             if edge.to not in visited:
-                if cost + edge.weight < minCost:
+                if minCost is None or cost + edge.weight < minCost:
                     minE = edge
                     minCost = cost + edge.weight 
-                    remPath = (path, cost)
+                    remPath = (pathSoFar, cost)
 
     paths.remove(remPath)
     
     return (remPath, minE)
 
 def findShortestPath(G, vStart, vEnd):
-    paths = [([vStart], 0)]
+    paths = []
     visited = set()
 
+    for edge in vStart.edges:
+        paths.append( ([vStart, edge.to], edge.weight) )
+        visited.add(vStart)
+
     while len(paths) > 0:
-
-        import pdb; pdb.set_trace()
-        for path in paths:
-            for v in path[0]:
-                print(v)
-            print('--x--')
-            
-
         path, edge = popLeastCostPathAndEdge(paths, visited)
         visitVertex(paths, path, edge, visited)
 
@@ -59,13 +52,22 @@ def findShortestPath(G, vStart, vEnd):
 
     raise Exception("Shortest path error")
 
-with open('undirected_weighted_graph_simple.dot', 'r') as f:
-    dotStr = f.read()
+def test(filename):
+    print('Testing with file: %s.' % filename)
 
-G = graph.graphFromDotString(dotStr)
+    with open(filename, 'r') as f:
+        dotStr = f.read()
 
-vStart = G.vertices['a']
-vEnd = G.vertices['d']
-shortestPath = findShortestPath(G, vStart, vEnd)
-print(shortestPath)
+    G = graph.graphFromDotString(dotStr)
 
+    vStart = G.vertices['a']
+    vEnd = G.vertices['d']
+    shortestPath = findShortestPath(G, vStart, vEnd)
+
+    for v in shortestPath:
+        print(v)
+
+if __name__ == '__main__':
+    test('undirected_weighted_graph_simple.dot')
+    test('undirected_weighted_graph_simple2.dot')
+    test('undirected_weighted_graph_simple3.dot')
