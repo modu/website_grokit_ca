62a525d5ceb6b18d6d65789181fbf907214f6372
grokit <grokit@users.noreply.github.com>
2014-12-15 00:06:07 -0800

    Update png_to_mp4.py

--- ws_root/ws_django/spc/fractal_canvas/render/png_to_mp4.py
+++ ws_root/ws_django/spc/fractal_canvas/render/png_to_mp4.py
@@ -4,7 +4,7 @@ os.chdir('./movie')
 
 #cmd = "ffmpeg -framerate 1 -pattern_type glob -i '*.png' -c:v libx264 out.mp4"
 #cmd = "ffmpeg -f image2 -i out_%05d.png -q:v 1 ../a.mpg"
-cmd = "convert -delay 3 -quality 100 *.png ../movie.mp4"
+cmd = "convert -delay 3 -quality 100 *.png ../movie.mp4" #.gif works as well
 
 print(cmd)
 
