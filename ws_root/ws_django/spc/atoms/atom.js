1019cdc9a56ab67a83a70f2373476850e559514c
david <reachme.web@outlook.com>
2014-12-25 21:54:06 -0800

    better

--- ws_root/ws_django/spc/atoms/atom.js
+++ ws_root/ws_django/spc/atoms/atom.js
@@ -18,10 +18,23 @@ Planet.prototype.draw = function(){
 createWorldObjects = function(world){
     world.objects.push(new Planet(100, 100));
     world.objects.push(new Planet(150, 150));
+    //world.objects.push(new Planet(350, 150));
 };
 
 var dist = function(o1, o2){
     return Math.sqrt( (o1.x - o2.x)*(o1.x - o2.x) + (o1.y - o2.y)*(o1.y - o2.y) );
+};
+
+var direction = function(o1, o2){
+    var v = new Object();
+    v.x = o2.x-o1.x;
+    v.y = o2.y-o1.y;
+
+    magn = Math.sqrt( v.x*v.x + v.y*v.y );
+    v.x = v.x/magn;
+    v.y = v.y/magn;
+    console.log(v);
+    return v;
 };
 
 updateLogic = function(world){
@@ -33,10 +46,12 @@ updateLogic = function(world){
                 var o1 = world.objects[j];
                 var o2 = world.objects[i];
                 var d = dist(o1, o2);
+                var v = direction(o1, o2);
 
-                if(d != 0){
-                    var attr = 1 / d; 
-                    o1.x = o1.x + attr*100;
+                if(d > 1){
+                    var attr = 1 / d*d; 
+                    o1.x += v.x*attr;
+                    o1.y += v.y*attr;
                 }
             }
         }
