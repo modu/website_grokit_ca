ad25ff1023fe7dd788e80a3e55fe203410591297
david <reachme.web@outlook.com>
2015-01-15 22:48:12 -0800

    ...

--- articles/construction/ThreadsLockingAndSynchronization/03_wait_and_signal.cc
+++ articles/construction/ThreadsLockingAndSynchronization/03_wait_and_signal.cc
@@ -9,50 +9,60 @@ class Counter
 {
 public:
     static const int N = 10000;
-    unique_ptr<mutex> counter_lock;
-    unique_ptr<condition_variable> cv;
-    unique_lock<mutex> lock1; // (*counter_lock, defer_lock); 
+    //unique_ptr<mutex> counter_lock;
+    //unique_ptr<condition_variable> cv;
+    //unique_lock<mutex> lock1; // (*counter_lock, defer_lock); 
+
+    unique_ptr<mutex> lock;
 
     Counter()
     {
-        counter_lock = unique_ptr<mutex>(new mutex());
-        cv = unique_ptr<condition_variable>(new condition_variable());
-        // defer_lock just means that it does not try to grab the lock at object creation.
-        lock1 = unique_lock<mutex>(*counter_lock, defer_lock);
+        unique_lock<mutex> signal_lock = unique_lock<mutex>(*unique_ptr<mutex>(new mutex()));
+      unique_ptr<condition_variable>  cv = unique_ptr<condition_variable>(new condition_variable());
+        //// defer_lock just means that it does not try to grab the lock at object creation.
+        //lock1 = unique_lock<mutex>(*counter_lock, defer_lock);
+        //
+        //oooasd
+
+        lock = unique_ptr<mutex>(new mutex());
     }
 
     void add()
     {
-        lock1.lock();
+        lock->lock();
 
-        if(val > N)
-        {
-            cout << "Too high! v: " << val << endl;
-            cout.flush();
-            //lock1.unlock();
-            //cv->wait(lock1);
-        }
+        //if(val > N)
+        //{
+        //    cout << "Too high! v: " << val << endl;
+        //    cout.flush();
+        //    lock->unlock();
+        //    cv->wait(lock2);
+        //    lock->lock();
+        //}
 
         ++val;
-        cv->notify_all();
-        lock1.unlock();
+        lock->unlock();
+        
+        //cv->notify_all();
     }
 
     void remove()
     {
-        lock1.lock();
+        lock->lock();
 
-        if(val <= 0)
-        {
-            cout << "Too low! v: " << val << endl;
-            cout.flush();
-            //lock1.unlock();
-            // cv->wait(lock1);
-        }
+        //if(val <= 0)
+        //{
+        //    cout << "Too low! v: " << val << endl;
+        //    cout.flush();
+        //    lock->unlock();
+        //    cv->wait(lock2);
+        //    lock->lock();
+        //}
 
         --val;
-        cv->notify_all();
-        lock1.unlock();
+        lock->unlock();
+
+        //cv->notify_all();
     }
 
     int val = 0;
@@ -64,19 +74,27 @@ void fn1(Counter& counter)
     {
         counter.add();
     }
+
+    // cout<< "thread done: " << counter.val << endl;
+}
+
+void tests()
+{
+    for(int i = 0; i < 10; ++i)
+    {
+        Counter counter = Counter();
+        thread t1(fn1, ref(counter));
+        thread t2(fn1, ref(counter));
+        t1.join();
+        t2.join();
+        cout << counter.val << endl;
+    }
 }
 
 int main()
 {
-    Counter counter = Counter();
-
-    thread t1(fn1, ref(counter));
-    thread t2(fn1, ref(counter));
-    t1.join();
-    t2.join();
-
-    cout << counter.val << endl;
-
+    cout << "start" << endl;
+    tests();
     return 0;
 }
 
