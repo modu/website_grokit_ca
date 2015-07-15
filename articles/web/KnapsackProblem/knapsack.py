7b48a12350f9606c90c96490d09c1a9fc73ad87f
arch2 <reachme.web@outlook.com>
2015-07-14 22:41:48 -0700

    nicer py

--- articles/web/KnapsackProblem/knapsack.py
+++ articles/web/KnapsackProblem/knapsack.py
@@ -14,28 +14,26 @@ import copy
 
 def readProblem(filename):
     fh = open(filename)
-    W, n = [int(x) for x in fh.readline().strip().split(' ')]
+    currWeight, n = [int(x) for x in fh.readline().strip().split(' ')]
 
     I = []
     for i in range(n):
-        X = [x for x in fh.readline().strip().split(' ')]
-        X[0] = int(X[0])
-        X[1] = int(X[1])
+        [(int(x), int(y)) for x, y in [fh.readline().strip().split(' ')]]
         I.append(X)
 
-    return (W, I)
+    return (currWeight, I)
 
 
-def recurse(i, I, S, W, memo):
+def recurse(i, I, S, currWeight, memo):
 
-    if memo.get((i, W)) is not None:
-        return memo[(i, W)]
+    if memo.get((i, currWeight)) is not None:
+        return memo[(i, currWeight)]
 
     if i == len(I):
         return 0, []
 
-    if W - I[i][1] >= 0:
-        cPick, pathP = recurse(i + 1, I, S, W - I[i][1], memo)
+    if currWeight - I[i][1] >= 0:
+        cPick, pathP = recurse(i + 1, I, S, currWeight - I[i][1], memo)
         cPick += I[i][0]
         # The pathP and such are not necessary to solve the problem, it is
         # there so that we can check the path.
@@ -45,7 +43,7 @@ def recurse(i, I, S, W, memo):
         cPick = -int(1e20)
 
     S.remove(i)
-    cNoPick, pathNp = recurse(i + 1, I, S, W, memo)
+    cNoPick, pathNp = recurse(i + 1, I, S, currWeight, memo)
     S.add(i)
 
     if cNoPick >= cPick:
@@ -53,7 +51,7 @@ def recurse(i, I, S, W, memo):
     else:
         sol = [cPick, pathP]
 
-    memo[(i, W)] = sol
+    memo[(i, currWeight)] = sol
 
     return sol
 
@@ -62,9 +60,9 @@ def solve(I, W):
     S = set(range(len(I)))
     return recurse(0, I, S, W, {})
 
-filename = 'knapsack_large.in'
-#filename = 'knapsack_small.in'
-#filename = 'k_tiny.in'
+# filename = 'knapsack_large.in'
+# filename = 'knapsack_small.in'
+filename = 'k_tiny.in'
 
 W, I = readProblem(filename)
 
