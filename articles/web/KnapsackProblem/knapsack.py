90fceb4b488dba6525f4049af1795780bb5a64b9
arch2 <reachme.web@outlook.com>
2015-07-14 23:42:53 -0700

    ++

--- articles/web/KnapsackProblem/knapsack.py
+++ articles/web/KnapsackProblem/knapsack.py
@@ -30,26 +30,22 @@ def recurse(i, I, S, currWeight, memo):
         return memo[(i, currWeight)]
 
     if i == len(I):
-        return 0, []
+        return 0
 
     if currWeight - I[i][1] >= 0:
-        cPick, pathP = recurse(i + 1, I, S, currWeight - I[i][1], memo)
+        cPick = recurse(i + 1, I, S, currWeight - I[i][1], memo)
         cPick += I[i][0]
-        # The pathP and such are not necessary to solve the problem, it is
-        # there so that we can check the path.
-        pathP = copy.copy(pathP)
-        pathP.append(i)
     else:
         cPick = -int(1e20)
 
     S.remove(i)
-    cNoPick, pathNp = recurse(i + 1, I, S, currWeight, memo)
+    cNoPick = recurse(i + 1, I, S, currWeight, memo)
     S.add(i)
 
     if cNoPick >= cPick:
-        sol = [cNoPick, pathNp]
+        sol = cNoPick
     else:
-        sol = [cPick, pathP]
+        sol = cPick
 
     memo[(i, currWeight)] = sol
 
@@ -60,17 +56,12 @@ def solve(I, W):
     S = set(range(len(I)))
     return recurse(0, I, S, W, {})
 
-# filename = 'knapsack_large.in'
-# filename = 'knapsack_small.in'
-filename = 'k_tiny.in'
+#filename = 'knapsack_large.in'
+filename = 'knapsack_small.in'
+#filename = 'k_tiny.in'
 
 W, I = readProblem(filename)
 
 s = solve(I, W)
 print(s)
 
-t = 0
-for p in s[1]:
-    print('%i: %s' % (p, I[p]))
-    t += I[p][0]
-print('Total: %i.' % t)
